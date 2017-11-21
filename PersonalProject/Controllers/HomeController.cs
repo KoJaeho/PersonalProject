a988e2fdebee85aa56fd357963604aab53e60b15
JaeHo Ko <jh.ko@newen.co.kr>
2017-11-21 18:52:50 +0900

    - 기본 웹 프로젝트 생성

---
+++ PersonalProject/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace PersonalProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
